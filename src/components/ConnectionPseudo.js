7857f037b3875db342e6eeb2e2025c524b30d2ae
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-04-14 17:16:36 +0200

    create a component that encompasses Indicator + PseudoName

---
+++ src/components/ConnectionPseudo.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Indicator from './Indicator';
+import PseudoName from './PseudoName';
+
+
+const ConnectionPseudo = () => {
+    return(
+        <div>
+            <Indicator />
+            <PseudoName />
+        </div>
+    )
+}
+
+export default ConnectionPseudo;
