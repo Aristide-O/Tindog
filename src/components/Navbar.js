ab5f61149fff286a4e31e2d7932cbaae61324a09
ariomega <aristide.odrg@gmail.com>
2020-04-15 18:34:58 +0200

    add a fake burger menu to navbar and add responsive media queries for moble version

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import message from '/Users/ariomega/Desktop/Tindog/paris-0320-prject2-tindog/sr
 import like from '/Users/ariomega/Desktop/Tindog/paris-0320-prject2-tindog/src/icons-navbar/like.png';
 import map from '/Users/ariomega/Desktop/Tindog/paris-0320-prject2-tindog/src/icons-navbar/folding-map (1).png';
 import profile from '/Users/ariomega/Desktop/Tindog/paris-0320-prject2-tindog/src/icons-navbar/user-image-with-black-background (1).png';
-
+import burger from '/Users/ariomega/Desktop/Tindog/paris-0320-prject2-tindog/src/icons-navbar/open-menu (1).png'
 
 function Navbar() {
     return (
@@ -21,6 +21,9 @@ function Navbar() {
         <a href='' ><img className='img-pawprint-navbar' src={like} alt='logo'/></a>
         <a href='' ><img className='img-pawprint-navbar' src={map} alt='logo'/></a>
         <a href='' ><img className='img-pawprint-navbar' src={profile} alt='logo'/></a>
+        </div>
+        <div className='icons-hidden'>
+        <a href='' ><img className='img-burger-navbar' src={burger} alt='logo'/></a>
         </div>
 </nav>
     )
