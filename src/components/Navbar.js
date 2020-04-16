e2e8eb1d82eb03c5dfb679d8724a0226f8529948
ariomega <aristide.odrg@gmail.com>
2020-04-16 19:06:16 +0200

    responsive mobile done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,43 @@
 import React from 'react';
-import  '/Users/ariomega/Desktop/Tindog/paris-0320-prject2-tindog/src/components/Navbar.css';
-import pawprint from '/Users/ariomega/Desktop/Tindog/paris-0320-prject2-tindog/src/icons-navbar/pawprint (1).png';
-import home from '/Users/ariomega/Desktop/Tindog/paris-0320-prject2-tindog/src/icons-navbar/home.png';
-import message from '/Users/ariomega/Desktop/Tindog/paris-0320-prject2-tindog/src/icons-navbar/speech-bubble.png';
-import like from '/Users/ariomega/Desktop/Tindog/paris-0320-prject2-tindog/src/icons-navbar/like.png';
-import map from '/Users/ariomega/Desktop/Tindog/paris-0320-prject2-tindog/src/icons-navbar/folding-map (1).png';
-import profile from '/Users/ariomega/Desktop/Tindog/paris-0320-prject2-tindog/src/icons-navbar/user-image-with-black-background (1).png';
-import burger from '/Users/ariomega/Desktop/Tindog/paris-0320-prject2-tindog/src/icons-navbar/open-menu (1).png'
+import  '../components/Navbar.css';
+import pawprint_navbar from '../icons-navbar/pawprint (1).png';
+import home from '../icons-navbar/home.png';
+import message from '../icons-navbar/speech-bubble.png';
+import like from '../icons-navbar/like.png';
+import map from '../icons-navbar/folding-map (1).png';
+import profile from '../icons-navbar/user-image-with-black-background (1).png';
+import burger from '../icons-navbar/open-menu (1).png'
+
 
 function Navbar() {
     return (
         <nav className='Navbar'>
-        <div className='tinder-navbar'>
-        <img className='img-pawprint-navbar' src={pawprint} alt='logo'/>
-        <p className='name-navbar'>Tindog</p>
-        </div>
-        <div className='icons-navbar'>
-        <a href='' ><img className='img-pawprint-navbar' src={home} alt='logo'/></a>
-        <a href='' ><img className='img-pawprint-navbar' src={message} alt='logo'/></a>
-        <a href='' ><img className='img-pawprint-navbar' src={like} alt='logo'/></a>
-        <a href='' ><img className='img-pawprint-navbar' src={map} alt='logo'/></a>
-        <a href='' ><img className='img-pawprint-navbar' src={profile} alt='logo'/></a>
-        </div>
-        <div className='icons-hidden'>
-        <a href='' ><img className='img-burger-navbar' src={burger} alt='logo'/></a>
-        </div>
-</nav>
+          <div className='tinder-navbar'>
+           <img className='img-pawprint-navbar' src={pawprint_navbar} alt='logo'/>
+           <p className='name-navbar'>Tindog</p>
+          </div>
+
+          <div>
+          <p className='name-navbar-hidden'>Tindog</p>
+          </div>
+
+         <div>
+
+          <div className='icons-navbar'>
+           <a href='' ><img className='img-icons-navbar' src={home} alt='home'/></a>
+           <a href='' ><img className='img-icons-navbar' src={message} alt='message'/></a>
+           <a href='' ><img className='img-icons-navbar' src={like} alt='like'/></a>
+           <a href='' ><img className='img-icons-navbar' src={map} alt='map'/></a>
+           <a href='' ><img className='img-icons-navbar' src={profile} alt='profile'/></a>
+          </div>
+
+          <div className='icons-hidden'>
+           <a href='' ><img className='img-burger-navbar' src={burger} alt='menu'/></a>
+          </div>
+          </div>
+
+
+         </nav>
     )
 
 }
