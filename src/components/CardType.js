cf29509fc303b1502f5a09ae6d30904b31d1aaa5
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-04-16 12:50:23 +0200

    add tag image + hard text in the description

--- src/components/CardType.js
+++ src/components/CardType.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './CardType.css'
 import ConnectionPseudo from './ConnectionPseudo';
-import TextDescriptionUser from './TextDescriptionUser'
 
 const CardType = () => {
     return(
         <div className="background-card">
-            <div className="textGlobal-card">
-            <ConnectionPseudo />
-            <TextDescriptionUser />
+                <img className="img-card" src="https://tractive.com/static/images/menu/Tractive-DOG-navigation.jpg" alt="test"></img>
+                <div className="text-total">
+                <ConnectionPseudo />
+                <p className="textDescription">Hi ! I am a sweet doggy and i am here to make friends and more if affinities</p>
+                </div>
             </div>
-        </div>
     )
 }
 
