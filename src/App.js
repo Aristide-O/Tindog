a031a50c7a886fcbaa0a4268ed0814339d2c8e78
MANAUT <tristanmanaut@gmail.com>
2020-04-17 10:31:46 +0200

    resolve conflits

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from "../components/Navbar.js"
+import Navbar from "./components/Navbar.js"
 import './App.css';
 import './components/Footer/Footer.css';
 import Footer from  "./components/Footer/Footer"
