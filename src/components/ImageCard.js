514b3c2706d14ed29c0e52f7f668726a2fe639e5
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-04-20 12:28:04 +0200

    add method and API key

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -7,14 +7,26 @@ class ImageCard extends React.Component {
     }
 
 getImage() {
-    axios
-    .get("")
-    .then(res => this.setState({ image : res.data}))
+    axios({
+        method: "get",
+        url: 'https://api.thedogapi.com/v1/images/search',
+        headers:{ 'x-api-key': "c05c2be9-d216-4f53-95de-6a5a06f0ae71" }
+        })
+    .then(res => this.setState({ image : res.data[0]}))
+}
+
+componentDidMount() {
+    this.getImage()
 }
 
     render() {
         return (
-            <img src={this.state.image} alt={this.state}/>
+            <div>
+                {
+                this.state.image ? <img className="img-card" src={this.state.image.url} alt={this.state.image.id}/> : <p>loading</p>
+                }
+            </div>
+            
         )
     }
 }
