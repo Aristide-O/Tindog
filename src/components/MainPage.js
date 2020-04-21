fc5e1fd267e65d5949479a97adaa9aaf52e9c4ed
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-04-21 15:02:20 +0200

    create mainpage

---
+++ src/components/MainPage.js
@@ -0,0 +1,19 @@
+// import React from "react";
+// import { Link } from "react-router-dom"
+
+// const MainPage = () => {
+//     return(
+//         <div>
+//             <nav>
+//                 <ul>
+//                     <li>
+//                         <Link to='/'/>
+//                     </li>
+//                 </ul>
+//             </nav>
+//         </div>
+        
+//     )
+// }
+
+// export default MainPage;
