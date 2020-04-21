5f9cf4891d1c98c27a3285b928e67fa842c401d9
ariomega <aristide.odrg@gmail.com>
2020-04-21 15:17:10 +0200

    css  match button compoonent

--- src/components/CardType.js
+++ src/components/CardType.js
@@ -5,17 +5,19 @@ import MatchButton from '../components/MatchButton'
 
 const CardType = () => {
     return(
+      <div className="flex-cardType">
         <div className="background-card">
-                <img className="img-card" src="https://tractive.com/static/images/menu/Tractive-DOG-navigation.jpg" alt="test"></img>
-                <div className="text-total">
+              <img className="img-card" src="https://tractive.com/static/images/menu/Tractive-DOG-navigation.jpg" alt="test"></img>
+              <div className="text-total">
                 <ConnectionPseudo />
                 <p className="textDescription">Hi ! I am a sweet doggy and i am here to make friends and more if affinities</p>
-                </div>
-                <div>
-                <MatchButton />
-                </div>
-            </div>
-
+              </div>
+              <MatchButton />
+        </div>
+        <div>
+         <MatchButton />
+        </div>
+      </div>
     )
 }
 
