98a5c5f68515a39bfe225416b62ef8d58a0122f7
Greta Mezzina <gretamezzina@Air-di-Greta>
2020-04-22 11:42:40 +0200

    error correction on InscriptionButton

--- src/components/InscriptionPage.js
+++ src/components/InscriptionPage.js
@@ -1,23 +1,24 @@
 import React from 'react';
+
+import Footer from "/Users/gretamezzina/Desktop/paris-0320-prject2-tindog/src/components/Footer/Footer.js"
 import InscriptionButton from './InscriptionButton';
 import LogInInformation from './LogInInformation';
-import './InscriptionPage.css';
-import { Link} from "react-router-dom";
 import Navbar from "/Users/gretamezzina/Desktop/paris-0320-prject2-tindog/src/components/Navbar.js";
-import Footer from "/Users/gretamezzina/Desktop/paris-0320-prject2-tindog/src/components/Footer/Footer.js"
+
+import './InscriptionPage.css';
 import '/Users/gretamezzina/Desktop/paris-0320-prject2-tindog/src/components/Footer/Footer.css'
 
 class InscriptionPage extends React.Component {
     render(){
     return (
-        <Link to="MainPage">
+        <div>
         <Navbar/>
         <div className="inscriptionPage">
             <LogInInformation/>
             <InscriptionButton/>
         </div>
         <Footer/>
-        </Link>
+        </div>
 )
     }
 }
