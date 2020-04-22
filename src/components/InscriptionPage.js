b59d28226647d186d2980e6125fd61a6e43750dd
Greta Mezzina <gretamezzina@Air-di-Greta>
2020-04-22 17:31:58 +0200

    import path modifiacted

--- src/components/InscriptionPage.js
+++ src/components/InscriptionPage.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import Footer from "/Users/gretamezzina/Desktop/paris-0320-prject2-tindog/src/components/Footer/Footer.js"
+import Footer from "./components/Footer/Footer.js"
 import InscriptionButton from './InscriptionButton';
 import LogInInformation from './LogInInformation';
-import Navbar from "/Users/gretamezzina/Desktop/paris-0320-prject2-tindog/src/components/Navbar.js";
+import Navbar from "./components/Navbar.js";
 
 import './InscriptionPage.css';
-import '/Users/gretamezzina/Desktop/paris-0320-prject2-tindog/src/components/Footer/Footer.css'
+import './components/Footer/Footer.css'
 
 class InscriptionPage extends React.Component {
     render(){
