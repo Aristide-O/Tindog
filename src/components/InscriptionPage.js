d052844a467206fd207f6f47a30395979192ab20
Greta Mezzina <gretamezzina@Air-di-Greta>
2020-04-22 11:38:26 +0200

    path done between signup and inscription page and between inscription page and mainpage

--- src/components/InscriptionPage.js
+++ src/components/InscriptionPage.js
@@ -2,15 +2,23 @@ import React from 'react';
 import InscriptionButton from './InscriptionButton';
 import LogInInformation from './LogInInformation';
 import './InscriptionPage.css';
+import { Link} from "react-router-dom";
+import Navbar from "/Users/gretamezzina/Desktop/paris-0320-prject2-tindog/src/components/Navbar.js";
+import Footer from "/Users/gretamezzina/Desktop/paris-0320-prject2-tindog/src/components/Footer/Footer.js"
+import '/Users/gretamezzina/Desktop/paris-0320-prject2-tindog/src/components/Footer/Footer.css'
 
 class InscriptionPage extends React.Component {
     render(){
     return (
+        <Link to="MainPage">
+        <Navbar/>
         <div className="inscriptionPage">
             <LogInInformation/>
             <InscriptionButton/>
         </div>
-    )
+        <Footer/>
+        </Link>
+)
     }
 }
 
