361639a4d57808781fb6fb43f9aa4ab6fd3aa63f
MANAUT <tristanmanaut@gmail.com>
2020-04-24 10:07:13 +0200

    change import in footer and inscription page

--- src/components/InscriptionPage.js
+++ src/components/InscriptionPage.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-import Footer from "./components/Footer/Footer.js"
+import Footer from "./Footer/Footer.js"
 import InscriptionButton from './InscriptionButton';
 import LogInInformation from './LogInInformation';
-import Navbar from "./components/Navbar.js";
+import Navbar from "./Navbar.js";
 
 import './InscriptionPage.css';
-import './components/Footer/Footer.css'
 
 class InscriptionPage extends React.Component {
     render(){
@@ -17,7 +16,7 @@ class InscriptionPage extends React.Component {
             <LogInInformation/>
             <InscriptionButton/>
         </div>
-        <Footer/>
+            <Footer/>
         </div>
 )
     }
