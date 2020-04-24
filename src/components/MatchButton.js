0642ab0d613c4687f94507925b675bb7901449ea
ariomega <aristide.odrg@gmail.com>
2020-04-24 12:14:55 +0200

    repositioning resizing  and change image of match button

--- src/components/MatchButton.js
+++ src/components/MatchButton.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-
-import match from '../icons-navbar/animal-care.png'
-
 import '../MatchButton.css'
 
 function MatchButton (props) {
