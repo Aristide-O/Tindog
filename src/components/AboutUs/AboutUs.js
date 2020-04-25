ea1c947e3678b6c87ed09e50cce483e4163bece1
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-04-25 14:07:29 +0200

    add burger right + lorem ipsum + url in array of object

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,10 +1,13 @@
 import React from "react"
+import Footer from "../Footer/Footer"
 import MembreType from "./MembreType";
-import AudreyImage from "equipe-tindog5.jpg";
-import AristideImage from "equipe-tindog2.jpg";
-import GretaImage from "equipe-tindog1.jpg";
-import JubaImage from "equipe-tindog3.jpg";
-import TristanImage from "equipe-tindog4.jpg";
+import Navbar from "../Navbar";
+import SideBar from "../SideBar";
+import AudreyImage from "./equipe-tindog3.png";
+import AristideImage from "./equipe-tindog2.png";
+import GretaImage from "./equipe-tindog1.png";
+import JubaImage from "./equipe-tindog.png";
+import TristanImage from "./equipe-tindog4.png";
 
 import './AboutUs.css';
 
@@ -12,27 +15,27 @@ import './AboutUs.css';
 const members = [
     {
         name : 'Greta',
-        image : {GretaImage},
+        image : {url : GretaImage},
         alt : "Avatar de Greta Mezzina"
     },
     {
         name : 'Aristide',
-        image : {AristideImage},
+        image : {url :AristideImage},
         alt : "Avatar de Aristide"
     },
     {
         name : 'Juba',
-        image : {JubaImage},
+        image : {url :JubaImage},
         alt : "Avatar de Juba Garidi"
     },
     {
         name : 'Tristan',
-        image : {TristanImage},
+        image : {url : TristanImage},
         alt : "Avatar de Tristan Manaut"
     },
     {
         name : 'Audrey',
-        image : {AudreyImage},
+        image : {url : AudreyImage},
         alt : "Avatar de Audrey Ritzenthaler"
     },
 ]
@@ -40,11 +43,25 @@ const members = [
 
 const AboutUs = () => {
     return(
-        <div className="equipe">
-            <h2 className="title-aboutus">About Us</h2>
-            {members.map((member, index) =>
-                 <MembreType key={index} image={member.image} name={member.name} alt={member.alt} />
-            )}
+        <div>
+            <SideBar right/>
+            <Navbar />
+            <h1 className="title-aboutus">About Us</h1>
+            <h2 className="team">Member's team</h2>
+            <div className="equipe">
+                {members.map((member, index) =>
+                    <MembreType key={index} image={member.image.url} name={member.name} alt={member.alt} />
+                )}
+            </div>
+            <div>
+                <p className="description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                <br />
+                <br />
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum !
+                </p>
+            </div>
+            <Footer />
         </div>
     )
 }
