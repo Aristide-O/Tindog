06aef09239ec0e27c28e1114680c086b4a41a56f
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-04-27 11:34:57 +0200

    add component ProfilePhoto

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Navbar from "../components/Navbar";
+import ProfilePhoto from "./ProfilePhoto";
 
 const MyProfile = () => {
     return(
         <div>
         <Navbar />
+        <ProfilePhoto />
             <p>JE SUIS SUR MON PROFIL</p>
         </div>
     )
