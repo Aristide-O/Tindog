22aa5a120b50a8695c81bbec85f19afebb42f0df
Aristide-O <heavenly_7@hotmail.fr>
2020-05-04 11:38:31 +0200

    add a message window in message.js

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,10 +1,12 @@
 import React from "react"
+import { Chat } from 'react-chat-popup';
 import Navbar from "../components/Navbar";
 
 const Message = (props) => {
     return(
         <div>
         <Navbar />
+        <Chat />
             <p>JE SUIS SUR MES MESSAGES</p>
             <p>Tu as WAAF {props.location.data && props.location.data.dogName.name.title} {props.location.data && props.location.data.dogName.name.first}</p> 
         </div>
