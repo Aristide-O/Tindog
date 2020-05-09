ea5351e498f1a59f2873b22e1f1cefa5da9b2b26
Greta Mezzina <gretamezzina@Air-di-Greta>
2020-05-09 16:36:39 +0200

    fix bug eliminating the modal and the labels on inscription page.

--- src/components/InscriptionButton.js
+++ src/components/InscriptionButton.js
@@ -6,53 +6,67 @@ import Modal from 'react-modal';
 import './InscriptionButton.css';
 
 class InscriptionButton extends React.Component {
-  
-    constructor(props) {
-    super(props);
-    this.state = {
-        showModal: false
-    }
-    }
+
+state = {
+            showModal: false
+        }
 
     handleOpenModal = () => {
         this.setState({ showModal: true });
     }
-    
+
     handleCloseModal = () => {
         this.setState({ showModal: false });
     }
 
     render() {
-        return (
-            <div className="inscriptionButton-block1">
-                <button className="inscriptionButton" type="send" onClick={this.handleOpenModal}>Sign-up</button>
-                <Modal
-                    className="modal"
-                    isOpen={this.state.showModal}
-                    contentLabel="Minimal Modal Example"
-                >
-                    <div className="modalHeader">
-                        <h2>BauBau!!</h2>
-                    </div>
-                    <div className="modalBody">
-                        <h3>Congratulations! </h3>
-                            <p>You are now on Tindog. Have fun smelling arounf!</p>
-                    </div>
-                    <Link to={{
-                      pathname : "/myprofile",
-                      data:{
-                      pseudo: this.props.pseudo,
-                      avatar: this.props.avatar,
-                      location: this.props.location,
-                      gender: this.props.gender,
-                      description: this.props.description
-                    }
-                  }}>
-                    <button className="inscriptionButton-closeModal" onClick={this.handleCloseModal}>Go tindogging</button>
-                    </Link>
-                </Modal>
-            </div>
-        );
+        return this.props.pseudo ?
+            (
+                <div className="inscriptionButton-block1">
+                    <button className="inscriptionButton" type="send" onClick={this.handleOpenModal}>Sign-up</button>
+                    <Modal
+                        className="modal"
+                        isOpen={this.state.showModal}
+                        contentLabel="Minimal Modal Example"
+                    >
+                        <div className="modalHeader">
+                            <h2>BauBau!!</h2>
+                        </div>
+                        <div className="modalBody">
+                            <h3>Congratulations! </h3>
+                            <p>You are now on Tindog. Have fun smelling around!</p>
+                        </div>
+                        <Link to={{
+                            pathname: "/myprofile",
+                            data: {
+                                pseudo: this.props.pseudo,
+                                avatar: this.props.avatar,
+                                location: this.props.location,
+                                gender: this.props.gender,
+                                description: this.props.description
+                            }
+                        }}>
+                            <button className="inscriptionButton-closeModal" onClick={this.handleCloseModal}>Go tindogging</button>
+                        </Link>
+                    </Modal>
+                </div>
+            )
+            : (
+                <div className="inscriptionButton-block1">
+                    <button className="inscriptionButton" type="send" onClick={this.handleOpenModal}>Sign-up</button>
+                    <Modal
+                        className="modalForm"
+                        isOpen={this.state.showModal}
+                        contentLabel="Minimal Modal Example"
+                    >
+                        <div className="modalFormBody">
+                            <h3>Please fill your information</h3>
+                            <button className="inscriptionButton-closeModal-2 topright" onClick={this.handleCloseModal}></button>
+                        </div>
+                    </Modal>
+                </div>
+
+            )
     }
 }
 
