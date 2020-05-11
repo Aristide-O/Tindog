14f9310dac8b399d47c82c5cffae5e3a104bff37
Aristide-O <heavenly_7@hotmail.fr>
2020-05-11 11:57:32 +0200

    new message page

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,21 +1,36 @@
 import React, { Component } from "react"
 import Navbar from "./Navbar";
-import SendMessage from "./SendMessage"
 import ContactList from "./ContactList"
 import Footer from "./Footer/Footer";
-import MessageArea from "./MessageArea"
+import MessageForm from './MessageForm'
+import MessageList from "./MessageList"
+
 
 import "./Message.css"
 
 
-class Message extends Component {
+class Message extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            messages: [],
+        }
+      } 
+
+      handleNewMessage = (text) => {
+        this.setState({
+          messages: [...this.state.messages, { me: true, author: "Me", body: text }],
+        })
+      }
+
     render() {
         return (
             <div>
                 <Navbar />
                 <div className="Message">
                     <ContactList />
-                    <MessageArea />
+                    <MessageList messages={this.state.messages} />
+                    <MessageForm onMessageSend={this.handleNewMessage} />
                 </div>
                 <Footer />
             </div>
