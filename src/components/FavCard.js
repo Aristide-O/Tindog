b3896ae988d6767a855dd8cb6ed19822f721279b
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-05-12 15:19:38 +0200

    add props

--- src/components/FavCard.js
+++ src/components/FavCard.js
@@ -1,10 +1,12 @@
 import React from "react"
+import Indicator from "./Indicator"
 
 const FavCard = (props) => {
     return(
         <div className="background-card">
             <img className="img-card" src={props.image} alt={props.name} />
-            <div className="text-total">
+            <div className="co-pseudo">
+                <Indicator />
                 <p className="textDescription">{props.name}</p>
             </div>
         </div>
